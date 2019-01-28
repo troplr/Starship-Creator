c6151a1b020a39382416cd5c0fe73609efa3253d
Sandeep Tete <sandeep.tete2011@gmail.com>
2019-01-28 14:44:03 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,27 +1,70 @@
-import os
+# -*- coding: utf-8 -*-
 from setuptools import setup
+from setuptools.command.install import install
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+import os
+
+# this is quic and dirty solution to ease multi-name distribution on PyPI
+try:
+    from requirements_txt import package_name
+except ImportError:
+    package_name = 'requirements-dev.txt'
+
+
+class AbortInstall(install):
+    def run(self):
+        raise SystemExit(
+            "It looks like you meant to type "
+            "`pip install -r %s`, but you left out the `-r` "
+            "by accident. Aborting installation." % package_name
+        )
+
+
+try:
+    from pypandoc import convert
+
+    def read_md(f):
+        return convert(f, 'rst')
+
+except ImportError:
+    convert = None
+    print(
+        "warning: pypandoc module not found, could not convert Markdown to RST"
+    )
+
+    def read_md(f):
+        return open(f, 'r').read()  # noqa
+
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+
 
 setup(
-    name = "star_ship_creator",
-    version = "0.0.4",
-    author = "Sandeep Tete",
-    author_email = "sandeep.tete20111@gmail.com",
-    description = ("An demonstration of how to create, document, and publish "
-                                   "to the cheese shop a5 pypi.org."),
-    license = "BSD",
-    keywords = "example documentation tutorial",
-        packages=['star_ship_creator', 'tests'],
-    long_description=read('README'),
+    name=package_name,
+    version="1.0.0",
+    author='Michał Jaworski',
+    author_email='swistakm@gmail.com',
+    description='Safeguard against one particullar "pip install" typo',
+    long_description=read_md(README),
+    url='https://github.com/troplr/Starship-Creator/requirements.txt',
+    include_package_data=True,
+    py_modules=['requirements_txt'],
+    cmdclass={'install': AbortInstall},
+
+    license="BSD",
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
     ],
 )
