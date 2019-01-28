fa2f84fb0ab1c229bc9f14902817718cb8c31bb6
Sandeep Tete <sandeep.tete2011@gmail.com>
2019-01-28 14:37:18 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "star_ship_creator",
+    version = "0.0.4",
+    author = "Sandeep Tete",
+    author_email = "sandeep.tete20111@gmail.com",
+    description = ("An demonstration of how to create, document, and publish "
+                                   "to the cheese shop a5 pypi.org."),
+    license = "BSD",
+    keywords = "example documentation tutorial",
+        packages=['star_ship_creator', 'tests'],
+    long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+)
